0523ee19f91a40186110644a9981e0f03f3e5958
comficker <comficker@gmail.com>
2024-02-25 20:50:15 +0700

    OK

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink class="logo" to="/">2048</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView/>
+  <footer>
+    <p>© 2023 <a class="underline" href="https://twitter.com/Inno_Games">Inno Games Studio</a>. All rights reserved.</p>
+  </footer>
+</template>
+
+<style>
+.logo {
+  font-size: 64px;
+  color: #FFF;
+  font-weight: bold;
+  text-decoration: none;
+}
+
+footer {
+  text-align: center;
+  font-size: 12px;
+  padding: 16px 0;
+  color: #EEE;
+}
+
+footer a {
+  color: #FFF;
+}
+</style>
---
+++ src/views/BoardView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="board" tabIndex="1">
+      <div v-for="(r_item, r_i) in board.cells" :key="r_i">
+        <cell v-for="(c_item, c_i) in r_item" :key="c_i"/>
+      </div>
+      <tile-view v-for="(tile, i) in tiles" :tile="tile" :key="i"/>
+      <game-end-overlay :board="board" :onrestart="onRestart"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from "../components/Cell.vue";
+import TileView from "../components/TileView.vue";
+import GameEndOverlay from "../components/GameEndOverlay.vue";
+import { Board } from "../board.js";
+import { onMounted, onBeforeUnmount, ref, computed } from "vue";
+export default {
+  setup() {
+    const board = ref(new Board());
+    const handleKeyDown = (event) => {
+      if (board.value.hasWon()) {
+        return;
+      }
+      if (event.keyCode >= 37 && event.keyCode <= 40) {
+        event.preventDefault();
+        const direction = event.keyCode - 37;
+        board.value.move(direction);
+      }
+    };
+    const onRestart = () => {
+      board.value = new Board();
+    };
+    onMounted(() => {
+      window.addEventListener("keydown", handleKeyDown);
+    });
+    onBeforeUnmount(() => {
+      window.removeEventListener("keydown", handleKeyDown);
+    });
+    const tiles = computed(() => {
+      return board.value.tiles.filter((tile) => tile.value !== 0);
+    });
+    return {
+      board,
+      onRestart,
+      tiles,
+    };
+  },
+  components: {
+    Cell,
+    TileView,
+    GameEndOverlay,
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-items: center;
+  min-height: 75vh;
+}
+</style>
